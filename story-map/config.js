a4e93dac312ab33032b221b938ddf2a8c9a56558
Benjamin Vankat <bvankat@gmail.com>
2024-11-06 03:15:15 -0600

    Finishing up data and layers

--- story-map/config.js
+++ story-map/config.js
@@ -13,14 +13,14 @@ var config = {
 	title: 'Nebraska Local News Map 2024',
 	byline: 'Report by XXXXXXXX',
 	description: 'In the vastness of the Great Plains state of Nebraska, there are wide stretches with no local journalists or local news organizations and other parts brimming with local media access. This report and digital map provides the true look at local journalism access in Nebraska. Explore this site to understand: Who has access to local news and who does not and why you should care.',
-	footer: 'Source: source citations, etc.',
+	footer: '[optional] Source: source citations, etc.',
 	chapters: [
 		{
 			id: 'slide-1',
 			alignment: 'left',
 			hidden: false,
 			title: '',
-			description: '<p><strong>There are 584 full-time local journalists working in Nebraska.</strong></p><p>This map is shaded by the number of journalists per county. The blue and green areas have higher concentrations of full- and part-time journalists.</p>',
+			description: '<p><strong>There are 584 full-time local journalists working in Nebraska.</strong></p><p>Blue and green counties have higher concentrations of full- and part-time journalists.</p>',
 			location: {
 				center: [-98.53151, 40.98629],
 				zoom: 5,
@@ -45,17 +45,58 @@ var config = {
 			onChapterExit: [
 				 {
 					 layer: 'Journalists per County',
-					 opacity: 0,
+					 opacity: 0.2,
 					 duration: 1000
 				 }
 			]
 		},
 		{
 			id: 'slide-2',
+			alignment: 'left',
+			hidden: false,
+			title: '',
+			description: '<p><strong>Nebraska has 196 local and statewide news outlets.</strong></p>',
+			location: {
+				center: [-98.53151, 40.98629],
+				zoom: 5.6,
+				pitch: 20,
+				bearing: 0,
+				duration: 4000
+			},
+			mapAnimation: 'flyTo',
+			rotateAnimation: false,
+			callback: '',
+			onChapterEnter: [
+				 {
+					 layer: 'Journalists per County',
+					 opacity: 0.1,
+					 duration: 3000
+				 },
+				 {
+					 layer: 'Local and Statewide Outlets',
+					 opacity: 1,
+					 duration: 2000
+				 }
+			],
+			onChapterExit: [
+				 {
+					 layer: 'Journalists per County',
+					 opacity: 0,
+					 duration: 1000
+				 },
+				 {
+					 layer: 'Local and Statewide Outlets',
+					 opacity: 0,
+					 duration: 2000
+				 }
+			]
+		},
+		{
+			id: 'slide-3',
 			alignment: 'right',
 			hidden: false,
 			title: '',
-			description: '<p><strong>Eleven Nebraska counties have no local news organization based in that county.</strong></p><p>42,942 Nebraskans live in counties that don’t have any full-time or part-time local journalists.</strong>',
+			description: '<p><strong>Eleven Nebraska counties have no local news organization based in that county.</strong></p><p>There are 42,942 Nebraskans living in counties that don\'t have any full-time or part-time local journalists.</strong></p>',
 			location: {
 				center: [-96.710, 40.833],
 				zoom: 5.3,
@@ -71,14 +112,20 @@ var config = {
 					 duration: 2000
 				 }
 			],
-			onChapterExit: []
+			onChapterExit: [
+				{
+					 layer: 'Empty Counties',
+					 opacity: 0,
+					 duration: 2000
+				 }
+			]
 		},
 		{
-			id: 'slide-3',
+			id: 'slide-4',
 			alignment: 'left',
 			hidden: false,
-			title: '[Slide 3]',
-			description: '<p><strong>55 counties have two or fewer full-time local journalists, or about 58%</strong></p>',
+			title: '',
+			description: '<p><strong>55 counties have two or fewer full-time local journalists.</strong></p><p>That\'s 58% of the state\'s counties.</p>',
 			location: {
 				center: [-99.886, 42.103],
 				zoom: 5.7,
@@ -108,13 +155,13 @@ var config = {
 			]
 		},
 		{
-			id: 'slide-4',
+			id: 'slide-5',
 			alignment: 'right',
 			hidden: false,
-			title: '[Slide 4]',
-			description: '<p><strong>17 county seats have no local news organization.</strong></p>',
+			title: '',
+			description: '<p><strong>17 county seats have no local news organization.</strong></p><p>No local news organizations based in: Alliance, Bartlett, Beaver City, Brewster, Center, Clay Center, Dakota City, Elwood, Tryon, Harrison, Harrisburg, Osceola, Papillion, Plattsmouth, Rushville, Stockville and Taylor.',
 			location: {
-				center: [-99.765, 40.768],
+				center: [-97.765, 40.768],
 				zoom: 5.5,
 				duration: 3000
 			},
@@ -123,21 +170,27 @@ var config = {
 			callback: '',
 			onChapterEnter: [
 				{
-					 layer: 'Nebraska Counties',
+					 layer: 'County Seats No LNO',
 					 opacity: 1,
-					 duration: 5000
+					 duration: 2000
 				 }
 			],
-			onChapterExit: []
+			onChapterExit: [
+				{
+					 layer: 'County Seats No LNO',
+					 opacity: 0,
+					 duration: 2000
+				 }
+			]
 		},
 		{
-			id: 'slide-5',
+			id: 'slide-6',
 			alignment: 'fully',
 			hidden: false,
 			title: '',
 			description: '[ Wrap up the map with some text about the report and what else people can explore. ]',
 			location: {
-				center: [-99.765, 40.768],
+				center: [-99.765, 42.768],
 				zoom: 5.3,
 				pitch: 20,
 				duration: 4000
